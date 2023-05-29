4ec6f1d7efe8aa83c412547a59e733da7a3f5b6d
tryphene10 <fofoumelachi@gmail.com>
2023-05-29 12:46:17 +0100

    database set up

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -113,43 +113,43 @@ class RegisterController extends BaseController
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
         }
 
-//        try {
-//            $validateUser = Validator::make($request->all(),
-//                [
-//                    'email' => 'required|email',
-//                    'password' => 'required'
-//                ]);
-//
-//            if($validateUser->fails()){
-//                return response()->json([
-//                    'status' => false,
-//                    'message' => 'validation error',
-//                    'errors' => $validateUser->errors()
-//                ], 401);
-//            }
-//
-//            if(!Auth::attempt($request->only(['email', 'password']))){
-//                return response()->json([
-//                    'status' => false,
-//                    'message' => 'Email & Password does not match with our record.',
-//                ], 401);
-//            }
-//
-//            $user = User::where('email', $request->email)->first();
-//
-//            return response()->json([
-//                'status' => true,
-//                'message' => 'User Logged In Successfully',
-//                'token' => $user->createToken("API TOKEN")->plainTextToken,
-//                'data' => $user
-//            ], 200);
-//
-//        } catch (\Throwable $th) {
-//            return response()->json([
-//                'status' => false,
-//                'message' => $th->getMessage()
-//            ], 500);
-//        }
+    /* try {
+            $validateUser = Validator::make($request->all(),
+                [
+                    'email' => 'required|email',
+                    'password' => 'required'
+                ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken,
+                'data' => $user
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }*/
     }
 
     //logout function
@@ -169,7 +169,7 @@ class RegisterController extends BaseController
         }
         $objAuthRole = Role::where("id", $user->role_id)->first();
         if ($objAuthRole->name != 'eleve'){
-            $listUser = User::where('role_id', '=', 1)->get();
+            $listUser = User::where('role_id', '=', 1)->get()->paginate(20);
             return $this->sendResponse($listUser, 'liste des élèves');
         }else{
             return $this->sendError('02',['error'=>'Cette action nécéssite une autorisation.']);
